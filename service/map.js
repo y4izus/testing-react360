3b57c4840819bbedc7b28249d00360a59c5a51fb
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2018-07-23 10:32:14 +0200

    wip

--- service/map.js
+++ service/map.js
@@ -8,8 +8,16 @@ export const drawSound = coordinate =>
 export const hasReachExit = coordinate => {
   console.log(coordinate)
   const [x, y] = coordinate
-  return (x > WIDTH || x < 0) || (y > HEIGHT || y < 0)
+  return (x > WIDTH || x < -WIDTH) || (y > HEIGHT || y < -HEIGHT)
 }
 
-export const drawMap = map =>
-  map.forEach(drawSound)
+export const drawMap = map => map.forEach(drawSound)
+
+const distanceColide = (direction, position) => (maxDistance, mapCell) => {
+  const distance = mapCell[direction].value - position[direction.includes(['up', 'down']) === 'x' ? 0 : 1]
+  mapCell[axis].ty && distance < maxDistance ? distance : maxDistance
+}
+
+export const minDistanceColide = (direction, position, maxDistance) => {
+  return map.reduce(distanceColide(direction, position), ['up', 'down'].includes(direction) ? HEIGHT * 2 : WIDTH * 2)
+}
